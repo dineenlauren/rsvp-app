65f5d6d54274aa7bb6f8184eb97349fbef910b4a
dineen.lauren <35275339+dineenlauren@users.noreply.github.com>
2021-01-04 14:04:38 -0800

    reconfigure publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/rsvp-app/' : '/',
-  outputDir: 'docs',
+  publicPath: '',
+  outputDir: './docs/',
+  assetsDir: 'assets',
 };
